f7291d2ae4b37ab656cb7f20d47628af1d92e2e1
venki13n <venki13.n@gmail.com>
2018-09-11 00:52:22 +0530

    pushing Extent implementation

---
+++ NUnit.Tests1/Extent/ExtentBase.cs
@@ -0,0 +1,81 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1.Extent
+{
+    class ExtentBase
+    {
+        private static ExtentReports extentReports;
+        private static ExtentHtmlReporter htmlReporter;
+        private static Dictionary<int, ExtentTest> TestsInProgress = null;
+        private static readonly Object obj = new Object();
+        public static ExtentReports extentInstance
+        {
+            get
+            {
+                lock (obj)
+                {
+                    if (extentReports == null)
+                    { return getInstance(); }
+                    return extentReports;
+                }
+            }
+        }
+
+        private ExtentBase() { }
+        private static ExtentReports getInstance()
+        {
+            htmlReporter = new ExtentHtmlReporter(@"G:\htmlFile.html");
+            htmlReporter.LoadConfig(@"C:\Users\ABC\source\repos\SeleniumNUnitTest\NUnit.Tests1\Extent\extent-config.xml");
+            htmlReporter.Configuration().Theme = Theme.Dark;
+            htmlReporter.Configuration().DocumentTitle = "ArrowReport";
+            htmlReporter.Configuration().ReportName = "ArrowReport";
+            extentReports = new ExtentReports();
+            extentReports.AttachReporter(htmlReporter);
+            extentReports.AddSystemInfo("OS_NAME", "Windows 10");
+            extentReports.AddSystemInfo("HostMachine", "Local Machine");
+            return extentReports;
+        }
+
+        public static ExtentTest CreateTest(string TestName, string Category="")
+        {            
+            lock (obj)
+            {
+                TestsInProgress = new Dictionary<int, ExtentTest>();
+                ExtentTest test = extentInstance.CreateTest(TestName);
+                TestsInProgress.Add(Thread.CurrentThread.ManagedThreadId, test);
+                //Console.WriteLine("Thread :{0}, Test: {1}", TestsInProgress.Keys.ToString(), TestName);
+                if (!Category.Equals(""))
+                {
+                    test.AssignCategory(Category);
+                }
+                return test;              
+            }
+        }
+
+        public static ExtentTest GetTargetTest()
+        {
+           // Console.WriteLine("Thread is :" + TestsInProgress[Thread.CurrentThread.ManagedThreadId]);
+            lock (obj)
+                {
+                    //Console.WriteLine("Thread is :" + TestsInProgress[Thread.CurrentThread.ManagedThreadId].ToString());
+                    return TestsInProgress[Thread.CurrentThread.ManagedThreadId];
+                }
+            
+        }
+
+        public static void FlushExtent()
+        {        
+            extentInstance.Flush();
+            //extentInstance.AddSystemInfo("LastMinuteUpdate", "LateUpadte");
+        }
+    
+    }
+}
---
+++ NUnit.Tests1/Extent/ExtentTestMethods.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1.Extent
+{
+    [TestFixture, TestFixtureSource(typeof(MyFactoryClass), "TestCaseListString")]
+    [Parallelizable]
+    class ExtentTestMethods
+    {
+        public ExtentTestMethods(string s)
+        {
+
+        }
+        //[OneTimeSetUp]
+        //public void oneTIme()
+        //{
+        //    ExtentBase.extentInstance.AddSystemInfo("OneTIme","Set");
+        //}
+        [SetUp]
+        public void setUpExtent()
+        {
+            ExtentBase.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+        [Test]
+        public void methodTestExtent1()
+        {
+            Console.WriteLine("test Stared");
+            ExtentBase.GetTargetTest().Log(AventStack.ExtentReports.Status.Fail, "Loggers");
+        }
+        [Test]
+        public void methodTestExtent2()
+        {
+            Console.WriteLine("test Stared");
+            ExtentBase.GetTargetTest().Log(AventStack.ExtentReports.Status.Fail, "Loggers");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if(TestContext.CurrentContext.Test.Name.Contains("1"))
+                    ExtentBase.GetTargetTest().AssignCategory("Category1");
+           else if (TestContext.CurrentContext.Test.Name.Contains("2"))
+                ExtentBase.GetTargetTest().AssignCategory("Category2");
+            ExtentBase.FlushExtent();              
+        }
+    }
+}
